cccc497d175ee2c478e3db9092150140ab9e7ab3
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-24 19:39:10 -0300

    Exercitando

---
+++ website/components/gerenciamento/Usuario.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Anne</h1>
+</template>
--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -34,6 +34,13 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  components:{
+    dirs: [
+      '~/components',
+      '~/components/gerenciamento',
+      '~/components/autenticacao',
+    ]
+  },
 }
 
